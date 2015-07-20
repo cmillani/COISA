1f760a05dda7ceda0582e53b486efeb2a44494c5
cmillani <carloseduardomillani@gmail.com>
2015-07-20 19:05:40 -0300

    For loop

--- src/ARCH/i386_OS/OS_loader.c
+++ src/ARCH/i386_OS/OS_loader.c
@@ -12,32 +12,36 @@ int main(int argc, char * argv[])
 	FILE * binary;
 	int i = 0, j = 0;
 	uint8_t byte;
-	
-	if (argc < 2) 
+	int w;
+	for (w = 0; w < 10; w++)
 	{
-		printf("Expected file name, aborting\n");
-		return 1;
+		if (argc < 2) 
+		{
+			printf("Expected file name, aborting\n");
+			return 1;
+		}
+
+		binary = fopen(argv[1], "rb");
+
+		while(fread(&byte,sizeof(uint8_t),1,binary))
+		{
+			VM_memory[i] = byte;
+			i++;
+		}
+
+		fclose(binary);
+	#if MEASURING
+		time_t tic = clock();
+	#endif
+	#if RUN_VM
+		vm_cpu();
+	#endif
+	#if MEASURING	
+		time_t toc = clock();
+		printf("TIMESPENT.%ld\n", (toc - tic));
+	#endif
+		i = 0;
 	}
-
-	binary = fopen(argv[1], "rb");
-
-	while(fread(&byte,sizeof(uint8_t),1,binary))
-	{
-		VM_memory[i] = byte;
-		i++;
-	}
-
-	fclose(binary);
-#if MEASURING
-	time_t tic = clock();
-#endif
-#if RUN_VM
-	vm_cpu();
-#endif
-#if MEASURING	
-	time_t toc = clock();
-	printf("TIMESPENT.%ld\n", (toc - tic));
-#endif
 	
 	return 0;
 }
